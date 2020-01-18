b88afd74152a3d7cc6dc4649896aac84f89f9de3
madhavic1 <madhavi.0108@gmail.com>
2020-01-17 20:46:51 -0800

    backup changes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+$(document).ready(function(){
+   // $(".bg").css("background-image", "url('09weather_ss-slide-LLBC-jumbo.JPG')");
+
 var APIKey = "80153e75178279ff6dfdc2b1bc72f6a2";
 var city_name;
-var today;
-
+var current_date = moment(new Date());
+var err = 0;
 //funtion createCityNameButton() creates a button with every city name searched 
 function createCityNameButton()
 {
@@ -12,47 +15,48 @@ function createCityNameButton()
 
 }
 
-function futureForecast()
-{
-    //step:1 - grab current date
- 
- for(var i=0;i<5;i++)
- {
-    var today = moment(new Date()).add(i,"days");
-    console.log(today.format("MM/D/YYYY"));
-    
- }
-    //step2:   generate forecast for next 5 days
-    // loop for 5 times. in each iteration , take 1 date , take response from weather API for that date , display and move to the next date.
-}
-
 function showWeatherReport(event){
     event.preventDefault(); 
     city_name = $("#search-city").val().trim();
     // alert("city name "+city_name);
     createCityNameButton();
- //   futureForecast();
     $("#search-city").val("");
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid=" + APIKey;
 // alert(queryURL);
     $.ajax({
         url: queryURL,
-        method:"GET"
+        method:"GET",
+        error: function(xhr, status, error){
+            var errorMessage = xhr.status + ': ' + xhr.statusText
+   
+            err = xhr.status;
+            //alert('Error - ' + errorMessage);
+        }
 
     }).then(function(response){
         console.log(response);
 
+      console.log("error status ------------ "+err);
       
        // $("#dataDiv").empty(); //empty the existing child elements
         
+       var dt_field = response.dt;
+       console.log("dt field "+dt_field);
        
         var cityName = response.name;
         today = moment(new Date());
-        var cloud = $("<i>");
-        cloud.attr("class","fa fa-cloud");
-        cloud.css({"font-size":"24px","color":"lightgray"});
-        $("#city-name").text(cityName+" \("+today.format("MM/D/YYYY"+"\)")+" ");
-        $("#city-name").append(cloud);
+        $("#city-name").text(cityName+" \("+current_date.format("MM/D/YYYY"+"\)")+" ");
+        // $("#city-name").append(cloud);
+        var weather_icon = $("<img>");
+        var iconID = response.weather[0].icon;
+        console.log("iconID "+iconID);
+        var iconURL = "http://openweathermap.org/img/wn/"+iconID+"@2x.png";
+        weather_icon.attr("src",iconURL)
+                    .width("10%")
+                    .height("10%");
+         $("#city-name").append(weather_icon);
+         //box-shadow: rgba(168, 228, 99, 0.3) 0 1px 2px 0,
+         //rgba(60, 64, 67, 0.15) 0 1px 3px 1px;
         $("#currentWeatherDiv").css( "border", "1px solid lightgreen" );
         var k_temp = response.main.temp; //in kelvin
         var f_temp = (k_temp - 273.15) * 1.80 + 32; //converted to Faren height
@@ -66,56 +70,127 @@ function showWeatherReport(event){
         $("#wind-speed").text("Wind Speed: "+response.wind.speed);
         //UV Index
         
-    
+        
 
     });
-    // show5dayForecast();
+console.log("############ "+city_name);
+
+    show5dayForecast(city_name);
+    
 
 }
 
-function show5dayForecast(event)
-{    
-    event.preventDefault();
-    city_name = $("#search-city").val().trim();
+function show5dayForecast(city_name) {
 
-    
-   var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city_name+"&appid=" + APIKey;
-
-console.log("URL "+forecastURL);
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city_name + "&appid=" + APIKey;
 
     $.ajax({
-        url : forecastURL,
+        url: forecastURL,
         method: "GET"
-    }).then(function(response){
-        console.log(response);
-        // var forecastArray = response.list;
-        // console.log("array length--"+response.list.length); 
-        // for(let i=0;i<+forecastArray.length;i++)
-        // {
-        //    // console.log(forecastArray[i]);
-        // //Date
-          
-        //     var _date = moment(forecastArray[i].dt_txt);
-        //     console.log("date "+ _date.format("L"));
-            
-         //   dateEl.text(_date.format("L"));
-          //  $("#5dayForecastDiv").append(dateEl);
+    }).then(function (response) {
 
-                //date
-                //weather icon
-                //Temp:
-                //Humidity
-/*** <div class="card text-white bg-primary mb-3 mr-1" style="max-width: 18rem;">
-                
-                <div class="card-body">
-                  <h5 class="card-title">Primary card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-              </div> */
+        console.log(response);
+        var forecastArray = response.list;
+
+        var today = moment(new Date());
+        var day1 = [];
+        var day2 = [];
+        var day3 = [];
+        var day4 = [];
+        var day5 = [];
+
+        var d1 = today.add(1, 'days').format("L");
+        var d2 = today.add(1, 'days').format("L");
+        var d3 = today.add(1, 'days').format("L");
+        var d4 = today.add(1, 'days').format("L");
+        var d5 = today.add(1, 'days').format("L");
+
+        for (elem in forecastArray) {
+
+            var date1 = moment(forecastArray[elem].dt_txt).format("L");
+
+            switch (date1) {
+                case today.format("L"):
+                    break;
+                case d1:
+                    day1.push(forecastArray[elem]);
+                    break;
+                case d2:
+                    day2.push(forecastArray[elem]);
+                    break;
+                case d3:
+                    day3.push(forecastArray[elem]);
+                    break;
+                case d4:
+                    day4.push(forecastArray[elem]);
+                    break;
+                case d5:
+                    day5.push(forecastArray[elem]);
+                    break;
+                default:
+                    break;
+            }
+            today = current_date; // resets the date value after all additions of days to form d1 to d5 variable values
         }
-        
-        
+
+
+        // below block of code removes the existing nodes of parent div and 
+        //then creates a forecast widget for exch day by taking each day array as a parameter
+
+        $("#5dayForecastDiv").empty();
+        //var averages_day1 = calculateAverageValues(day1);
+        createForecastWidget(day1);
+        //var averages_day2 = calculateAverageValues(day2);
+        createForecastWidget(day2);
+        // var averages_day3 = calculateAverageValues(day3);
+        createForecastWidget(day3);
+        // var averages_day4 = calculateAverageValues(day4);
+        createForecastWidget(day4);
+        // var averages_day5 = calculateAverageValues(day5);
+        createForecastWidget(day5);
+
+
     });
-                
+
 }
-$("#search-btn").on("click",show5dayForecast);
+$("#search-btn").on("click",showWeatherReport);  //Event listener for search button  beside City Name input field
+
+function createForecastWidget(day) {
+
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    
+    console.log(day);
+    
+    var cardDiv = $("<div>");
+    cardDiv.attr("class", "card text-white bg-primary mb-3 mr-1 border-warning border-left-0 border-bottom-0");
+    var cardBodyDiv = $("<div>");
+    cardBodyDiv.attr("class", "card-body p-0");
+    var dateEl = $("<h5>");
+    dateEl.attr("class", "card-title");
+    dateEl.text(moment(day[1].dt_txt).format("L"));
+    var weather_icon = $("<img>");
+    var iconID = day[1].weather[0].icon;
+    console.log("iconID "+iconID);
+    var iconURL = "http://openweathermap.org/img/wn/"+iconID+"@2x.png";
+    weather_icon.attr("src",iconURL)
+                .width("40%")
+                .height("40%");
+    
+    var temperature = $("<p>");
+    temperature.attr("class", "card-text");
+    temperature.text("Temp: " + Number.parseFloat(day[1].main.temp).toFixed(2));
+    var humidity = $("<p>");
+    humidity.attr("class", "card-text");
+    humidity.text("Humidity: " + day[1].main.humidity);
+    cardBodyDiv.append(dateEl,weather_icon, temperature, humidity);
+    cardDiv.append(cardBodyDiv);
+    $("#5dayForecastDiv").append(cardDiv);
+}
+
+
+
+
+
+
+
+});
